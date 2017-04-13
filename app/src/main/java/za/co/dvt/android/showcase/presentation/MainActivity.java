c5461829caa1c6f8e59c8cd47e71497d36cc2e6d
Rebecca Franks <franks.rebecca@gmail.com>
2017-04-13 11:30:34 +0200

    Added FirebaseUI for app login.

--- app/src/main/java/za/co/dvt/android/showcase/presentation/MainActivity.java
+++ app/src/main/java/za/co/dvt/android/showcase/presentation/MainActivity.java
@@ -1,7 +1,9 @@
 package za.co.dvt.android.showcase.presentation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import za.co.dvt.android.showcase.R;
 
@@ -11,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static Intent createIntent(Context context) {
+        return new Intent(context, MainActivity.class);
     }
 }
