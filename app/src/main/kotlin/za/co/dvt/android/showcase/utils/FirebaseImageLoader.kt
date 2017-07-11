e5f7e9b2bbb41ccfcaabcac42d084419c83bea56
Rebecca Franks <franks.rebecca@gmail.com>
2017-07-11 09:23:52 +0200

    chore: fix constraint layout android studio bugs. Updated to new AS. Fixed some layout issues.

---
+++ app/src/main/kotlin/za/co/dvt/android/showcase/utils/FirebaseImageLoader.kt
@@ -0,0 +1,78 @@
+package za.co.dvt.android.showcase.utils
+
+/**
+ * @author rebeccafranks
+ * *
+ * @since 2017/06/29.
+ */
+
+import android.util.Log
+
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.data.DataFetcher
+import com.bumptech.glide.load.model.stream.StreamModelLoader
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StreamDownloadTask
+
+import java.io.IOException
+import java.io.InputStream
+
+/**
+ * ModelLoader implementation to download images from FirebaseStorage with Glide.
+ *
+ *
+ * Sample Usage:
+ * <pre>
+ * StorageReference ref = FirebaseStorage.getInstance().getReference().child("myimage");
+ * ImageView iv = (ImageView) findViewById(R.id.my_image_view);
+
+ * Glide.with(this)
+ * .using(new FirebaseImageLoader())
+ * .load(ref)
+ * .into(iv);
+</pre> *
+ */
+class FirebaseImageLoader : StreamModelLoader<StorageReference> {
+
+    override fun getResourceFetcher(model: StorageReference, width: Int, height: Int): DataFetcher<InputStream> {
+        return FirebaseStorageFetcher(model)
+    }
+
+    private inner class FirebaseStorageFetcher internal constructor(private val mRef: StorageReference) : DataFetcher<InputStream> {
+        private lateinit var mStreamTask: StreamDownloadTask
+        private lateinit var mInputStream: InputStream
+
+        @Throws(Exception::class)
+        override fun loadData(priority: Priority): InputStream {
+            mStreamTask = mRef.stream
+            mInputStream = Tasks.await<StreamDownloadTask.TaskSnapshot>(mStreamTask).stream
+
+            return mInputStream
+        }
+
+        override fun cleanup() {
+            try {
+                mInputStream.close()
+            } catch (e: IOException) {
+                Log.w(TAG, "Could not close stream", e)
+            }
+        }
+
+        override fun getId(): String {
+            return mRef.path
+        }
+
+        override fun cancel() {
+            // Cancel task if possible
+            if (mStreamTask.isInProgress) {
+                mStreamTask.cancel()
+            }
+        }
+    }
+
+    companion object {
+
+        private val TAG = "FirebaseImageLoader"
+    }
+}
