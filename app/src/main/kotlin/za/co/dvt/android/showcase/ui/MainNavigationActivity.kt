f48f84d525bbff2a7160b3f420b5320548a1f8be
Rebecca Franks <franks.rebecca@gmail.com>
2017-07-18 13:02:25 +0200

    Cleaned up look of items, fixed issue on rotate of screen where the fragment would disappear from the view.

--- app/src/main/kotlin/za/co/dvt/android/showcase/ui/MainNavigationActivity.kt
+++ app/src/main/kotlin/za/co/dvt/android/showcase/ui/MainNavigationActivity.kt
@@ -3,6 +3,7 @@ package za.co.dvt.android.showcase.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
@@ -12,6 +13,7 @@ import za.co.dvt.android.showcase.R
 import za.co.dvt.android.showcase.ui.about.AboutFragment
 import za.co.dvt.android.showcase.ui.contact.ContactUsFragment
 import za.co.dvt.android.showcase.ui.listapps.ListAppsFragment
+
 
 class MainNavigationActivity : AppCompatActivity() {
 
@@ -37,15 +39,28 @@ class MainNavigationActivity : AppCompatActivity() {
 
     }
 
+    private var selectedItem: Int = 0
+    private lateinit var navigationView: BottomNavigationView
+
+    private val SELECTED_FRAGMENT_BUNDLE = "selected_fragment"
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        selectedItem = navigationView.selectedItemId
+        outState?.putInt(SELECTED_FRAGMENT_BUNDLE, selectedItem)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_navigation)
 
         frameLayout = findViewById(R.id.fragment_content)
 
-        val navigation = findViewById<BottomNavigationView>(R.id.navigation)
-        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-        navigation.selectedItemId = R.id.navigation_home
+        navigationView = findViewById<BottomNavigationView>(R.id.navigation)
+        navigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        selectedItem = savedInstanceState?.getInt(SELECTED_FRAGMENT_BUNDLE) ?: R.id.navigation_home
+        navigationView.selectedItemId = selectedItem
     }
 
     fun showScreen(fragment: Fragment) {
