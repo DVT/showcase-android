1ff5871b09bb8b90ab24fa25be87cfc752889415
Rebecca Franks <franks.rebecca@gmail.com>
2017-09-14 16:09:04 +0200

    Updated versions of libraries, updated background images and art.

--- app/src/main/kotlin/za/co/dvt/android/showcase/ui/login/LoginActivity.kt
+++ app/src/main/kotlin/za/co/dvt/android/showcase/ui/login/LoginActivity.kt
@@ -14,7 +14,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setBackgroundDrawableResource(R.drawable.blue_background)
+        window.setBackgroundDrawableResource(R.drawable.background)
         setContentView(R.layout.activity_login_screen)
     }
 
